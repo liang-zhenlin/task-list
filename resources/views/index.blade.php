19f54525982afb8d2759c938b3d67701006beb16
liangzhenlin <mac@192.168.10.3>
2025-05-09 22:44:56 +0800

    add blade file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<div>The list of tasks</div>
+
+<div>
+    @forelse ($tasks as $task)
+        <div>
+
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
