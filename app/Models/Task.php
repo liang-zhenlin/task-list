1e7623e2b8d0201e0a57eef50cdf70febed0ac1e
liangzhenlin <mac@macdeMacBook-Pro.local>
2025-05-12 17:30:02 +0800

    add delete function and toggle-complete

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'long_description'];
+
+    public function toggleComplete() {
+        $this->completed = !$this->completed;
+        $this->save();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\TaskRequest;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -11,64 +12,58 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => Task::latest()->where('completed', true)->get()
+        'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}/edit', function ($id) {
+Route::get('/tasks/{task}/edit', function (Task $task) {
     return view('edit', [
-        'task' => Task::findOrFail($id)
+        'task' => $task
     ]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function ($id) {
-    return view('show', ['task' => Task::findOrFail($id)]);
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
 
 Route::get('/hallo', function () {
     return redirect('/hello');
 });
 
-Route::post('/tasks', function(Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-    return redirect()->route('tasks.show', ['id' => $task->id])
+Route::post('/tasks', function(TaskRequest $request) {
+    // $data = $request->validated();
+    // $task = new Task;
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+    $task = Task::create($request->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success','Task created successfully!');
 
 })->name('tasks.store');
 
 
-Route::put('/tasks/{id}', function($id, Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-    return redirect()->route('tasks.show', ['id' => $task->id])
+Route::put('/tasks/{task}', function(Task $task, TaskRequest $request) {
+    // $data = $request->validated();
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+    $task->update($request->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success','Task updated successfully!');
 
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function(Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')->with('success','Task deleted successfully!');
+})->name('tasks.destroy');
 
-// Route::get('/hello', function() {
-//     return 'Hello';
-// })->name('hello');
-
-// Route::get('greet/{name}', function($name) {
-//     return 'Hello ' . $name . '!';
-// });
+Route::put('/tasks/{task}/toggle-complete', function (Task $task) {
+    $task->toggleComplete();
+    return redirect()->back()->with('success','Task updated successfully!');
+})->name('tasks.toggle-complete');
